eda225241477d2a8edba580ddba2be55c27e154f
bubsy21-dev <119619859+bubsy21-dev@users.noreply.github.com>
2024-08-22 20:02:35 -0400

    Update general.h

--- config/general.h
+++ config/general.h
@@ -1,9 +1,9 @@
 #define QUICK_TAP_MS 175
 
-&sk {  // sticky-key config
-    release-after-ms = <600>;  // release after 0.6s
-    quick-release;             // no double capitalization when rolling keys
-};
+//&sk {  // sticky-key config
+    //release-after-ms = <600>;  // release after 0.6s
+    //quick-release;             // no double capitalization when rolling keys
+//};
 
 //&sl {  // sticky-layer config
     //ignore-modifiers;          // allow chording sticky mods & layers
